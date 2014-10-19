95132ddb6cce90804de2d64ac7cdc3e3aa74039d
Barney Potts <barney@goopot.co.uk>
2014-10-19 15:43:28 +0100

    Search working

--- scripts/twit.py
+++ scripts/twit.py
@@ -1,6 +1,7 @@
 from twitter import Twitter
 import ConfigParser as configparser
 from twitter import OAuth
+from time import sleep
 
 
 
@@ -21,12 +22,23 @@ def auth():
 def find_max_id(query, t):
     results = t.search.tweets(q=query, result_type="recent")
     return results["search_metadata"]["max_id"]
+
+
   
+def watch_for_tweet(query, t, max_id):
+    while True:    
+        results = t.search.tweets(q=query,
+                                  since_id=max_id,
+                                  result_type="recent")
+        if results["statuses"]:
+            print(results["statuses"][0]["text"])
+            break
+        sleep(5)
 
 
 if __name__ == "__main__":
-    query = "barney"
+    query = "python"
     t = auth()
     max_id = find_max_id(query, t)
-    # watch_for_tweet(query, max_id)
+    watch_for_tweet(query, t, max_id)
     print(max_id)
